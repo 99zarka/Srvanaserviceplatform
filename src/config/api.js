65ef7122795755437cfa41062249839a540522f4
99zarka <mohamedmanchestrawy@gmail.com>
2025-12-06 07:36:36 +0200

    new backend url

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
 const BASE_URL = import.meta.env.MODE === 'production'
-  ? import.meta.env.VITE_API_BASE_URL
+  ? "https://srvanabackend-268062404120.us-central1.run.app/api"
   : import.meta.env.VITE_LOCAL_API_BASE_URL;
 
 export default BASE_URL;
